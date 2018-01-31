0500065fe65385b53a6068b0b1412eddab1177df
konohanasakuya <princesskonohanasakuya@gmail.com>
2018-01-31 18:45:22 +0900

    A

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-/**
- * Responds to any HTTP request that can provide a "message" field in the body.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
-exports.helloWorld = function helloWorld (req, res) {
-  if (req.body.message === undefined) {
-    // This is an error case, as "message" is required
-    res.status(400).send('No message defined!');
-  } else {
-    // Everything is ok
-    // console.log(req.body.message);
-    //res.status(200).end();
-    res.status(200).send(req.body.message);
-  }
-}
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
+
+const ref = admin.database().ref()
+
+//Authentication Trigger
+exports.createUserAccount = functions.auth.user().onCreate(event => {
+    const uid = event.data.uid
+    const email =  event.data.email
+    const photoUrl = event.data.photoUrl || 
+    'photo_url'
+    const newUserRef = ref.child(`/users/${uid}`)
+    return newUserRef.set({
+        photoUrl: photoUrl,
+        email: email
+    })
+})
