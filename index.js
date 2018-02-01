f7b5aad2793baa6df164006d3b0a4c59e13166f3
konohanasakuya <princesskonohanasakuya@gmail.com>
2018-02-01 12:06:14 +0900

    TEST1

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase)
-
-const ref = admin.database().ref()
-
-//Authentication Trigger
-exports.createUserAccount = functions.auth.user().onCreate(event => {
-    const uid = event.data.uid
-    const email =  event.data.email
-    const photoUrl = event.data.photoUrl || 
-    'photo_url'
-    const newUserRef = ref.child(`/users/${uid}`)
-    return newUserRef.set({
-        photoUrl: photoUrl,
-        email: email
-    })
-})
+admin.initializeApp(functions.config().firebase);
+ 
+exports.product = functions.https.onRequest((request, response) => {
+ 
+    if (request.method === "GET") {
+        admin.database().ref("/product").once("value")
+            .then(snapshot => {
+                    const products = snapshot.val();
+                    const array = Object.keys(products).map(key => products[key]);
+ 
+                    console.log("reference success", array);
+                    response.status(200).json(array).end();
+                }
+            ).catch(error => {
+                console.log("reference error", error);
+                response.status(500).end();
+            }
+        );
+ 
+    } else if (request.method === "POST") {
+        const body = request.body;
+        const pushRef = admin.database().ref("/product").push();
+ 
+        pushRef.set({
+            name: body["name"],
+            price: body["price"]
+        }, error => {
+ 
+            if (error) {
+                console.log("save error", error.message);
+                response.status(500).send(error.message).end();
+            } else {
+                console.log("save success");
+                response.status(200).end();
+            }
+        });
+ 
+    } else {
+        response.status(404).end();
+    }
+});
